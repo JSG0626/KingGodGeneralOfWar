ed554701c5886f5b2d79713035a2efd50cf8b6be
JSG0626 <tmdrjs1419@naver.com>
2025-07-28 05:10:27 +0900

    <Fix> AN_SetState를 C++ 기반으로 재작성

---
+++ Source/KingGodGeneralOfWar/Private/AN_PlayerSetState.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AN_PlayerSetState.h"
+
+void UAN_PlayerSetState::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	Super::Notify(MeshComp, Animation);
+	if (!MeshComp || !MeshComp->GetWorld() || !MeshComp->GetWorld()->IsGameWorld())
+	{
+		return;
+	}
+	UAnimInstance* anim = MeshComp->GetAnimInstance();
+	if (anim == nullptr) return;
+
+	APawn* pawn = anim->TryGetPawnOwner();
+	if (pawn == nullptr) return;
+
+	AKratos* kratos = CastChecked<AKratos>(pawn);
+	kratos->SetState(NewState);
+}
---
+++ Source/KingGodGeneralOfWar/Public/AN_PlayerSetState.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include <Kratos.h>
+#include "AN_PlayerSetState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KINGGODGENERALOFWAR_API UAN_PlayerSetState : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere)
+	EPlayerState NewState;
+
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+};
